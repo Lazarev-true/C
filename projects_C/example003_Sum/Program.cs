e19d4fc7a560813026d647794c2b5bb4f8ae0901
Lazarev Konstantin <klik@example.com>
2022-08-19 09:28:57 +0300

    Выбор чисел случайным образом

--- projects_C/example003_Sum/Program.cs
+++ projects_C/example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 324;
-int numberB = 78;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(20, 30);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
